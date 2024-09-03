18ea128b4655d1d3d8829dac7ad45dc1b349efa5
Xavier Bruhiere <1517057+xav-b@users.noreply.github.com>
2024-09-03 23:32:29 +0800

    feat(services): content of the page

--- app/config/index.ts
+++ app/config/index.ts
@@ -1,3 +1,14 @@
+export function buildMailTo() {
+  const lineBreak = '%0D%0A%0D%0A'
+  const p = {
+    email: 'xavier-bruhiere@protonmail.com',
+    subject: "Let's explore working together",
+    body: `Hey Xavier,${lineBreak}`,
+  }
+
+  return `mailto:${p.email}?subject=${p.subject}&body=${p.body}`
+}
+
 // too keep in sync with `global.css`
 export const theme = {
   light: {
@@ -10,14 +21,15 @@ export const theme = {
 
 const config = {
   url: 'https://xavierbruhiere.com',
-  title: "Xavier Bruhiere's blog",
-  description: 'Xavier Bruhiere is an engineering leader and senior backend engineer at Binance',
+  title: 'Xavier Bruhiere - Blog',
+  description: 'Software Engineering, Backends, Leadership, Markets',
   source: 'https://github.com/xav-b/blog',
   author: {
     name: 'Xavier Bruhiere',
     username: 'xav-b',
     twitter: 'XavierBruhiere',
     connect: 'https://www.linkedin.com/in/engineerxbruhiere/',
+    // connect: buildMailTo(),
     email: 'xavier.bruhiere@gmail.com',
   },
   redis: {
